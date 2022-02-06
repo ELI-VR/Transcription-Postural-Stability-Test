0ba91a7babf5bd5179fdd17f62871f4db55dbb80
Yesid <yesidcanoc@gmail.com>
2022-02-06 14:04:03 -0500

    save text per condition to pickle file

--- NLP/text_analysis.py
+++ NLP/text_analysis.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import argparse
 import re
+from NLP.utils import handle_pickle
 # setting up CLI
 
 
+#TODO specify path to save pickle file
 
 parser = argparse.ArgumentParser(description = "Text analysis")
 parser.add_argument("transcription_csv", help = "path to the csv file containing the transcriptions")
@@ -12,9 +14,7 @@ args = parser.parse_args()
 
 
 df = pd.read_csv(args.transcription_csv)
-df_clean =df[['id',
-
-       'BlobFirstperson_0', 'BlobFirstperson_1', 'BlobFirstperson_2',
+df = df[['id','BlobFirstperson_0', 'BlobFirstperson_1', 'BlobFirstperson_2',
        'BlobFirstperson_4', 'BlobHybrid_0', 'BlobHybrid_1', 'BlobHybrid_2',
        'BlobHybrid_4', 'AvatarFirstperson_1', 'AvatarFirstperson_0',
        'BlobHybrid_3', 'BlobFirstperson_3', 'AvatarHybrid_1', 'AvatarHybrid_0',
@@ -26,7 +26,9 @@ def create_condition_txt(column):
     #TODO this can be implemented using map()
     text=''
     for cell in column:
-        text= text + '\n'+cell
+        if type(cell) != float:
+            text = f'{text}  \n {str(cell)}'
+            #text= text + '\n' + str(cell)
     return text
 
 def create_text (df_transcription):
@@ -38,22 +40,19 @@ def create_text (df_transcription):
     Returns:
 
     """
-    #TODO INCLUDE OPTION WHERE CELL CONTAINS nan or None
+
+    #this regex returns the name of the column
     column_regex = re.compile(r'([A-Za-z]*)\s?\_?([0-9]$)')
-
+    #list of the columns
     columns = df_transcription.columns
-    count=1
-    column_name= lambda count: column_regex.search(columns[count]).groups()[0]
-    current_column = column_name(count)
-    text=''
-    for column in df_transcription:
-        count =count +1
-        column_loop= column_name(count)
-        if current_column == column_loop:
-            create_condition_txt(df_transcription[column])
-        else:
-            current_column=column_loop
-            #do same thing
+    columns=columns[1:-1]
+    #column_name= lambda count: column_regex.search(columns[count]).groups()[0]
+    text={}
+    for column in columns:
+        text[column]=[create_condition_txt(df_transcription[column])]
 
-
+    handle_pickle('/home/yesid/Documents/Master_semester3/VR/data/Linus_transcription',text)
 create_text(df)
+
+handle_pickle('/home/yesid/Documents/Master_semester3/VR/data/Linus_transcription',open_file=True)
+print('')

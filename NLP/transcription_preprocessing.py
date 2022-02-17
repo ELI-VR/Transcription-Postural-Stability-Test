f287794333f603acfae59bfd49533c1930898b7d
Yesid <yesidcanoc@gmail.com>
2022-02-17 01:26:54 -0500

    implemented process_rated_transcription()

--- NLP/transcription_preprocessing.py
+++ NLP/transcription_preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import argparse
 import re
-from NLP.utils import handle_pickle
+from NLP.utils import handle_pickle, condition_id
 from NLP.utils import list_of_columns_transcription
 # setting up CLI
 
@@ -17,6 +17,61 @@ args = parser.parse_args()
 df = pd.read_csv(args.transcription_csv)
 df = df[list_of_columns_transcription]
 print('')
+
+
+
+def process_rated_transcription (path, output_dic):
+    """
+
+    Args:
+        path: path to csv file that contains the rated transcriptions.
+        output_dic: path to save clean_transcription.csv file
+    Returns:
+
+    """
+    dic_data ={
+        'id':[],
+        'rating':[],
+        'condition':[],
+        'mode':[],
+        'transcription':[]
+
+    }
+    condition_id_dictionary = {y:x for x,y in condition_id.items()}
+    df = pd.read_csv(path, sep=';')
+
+
+    column_id = re.compile(r'([0-9]{2})_([0-9]{2})')
+    for index, row in df.iterrows():
+        id_condition = column_id.search(row['id_transcription']).groups()
+        dic_data['id'].append(int(id_condition[0]))
+        dic_data['rating'].append([row['Rating']])
+        condition_mode_station = condition_id_dictionary[id_condition[1]]
+        if 'Blob' in condition_mode_station:
+            dic_data['condition'].append('Blob')
+        else:
+            dic_data['condition'].append('Avatar')
+        if 'Hybrid' in condition_mode_station:
+            dic_data['mode'].append('Hybrid')
+        else:
+            dic_data['mode'].append('Firstperson')
+
+        dic_data['transcription'].append(row['trascription'])
+
+        print('')
+    df = pd.DataFrame.from_dict(dic_data).sort_values(by=['id'])
+    # Exports information to a csv file after having sorted out by id.
+    df.to_csv(output_dic + '/clean_transcription.csv', index=False)
+
+
+
+
+process_rated_transcription('/home/yesid/Documents/Master_semester3/VR/data/Linus_transcription/rated_transcription/df_0 processed.csv')
+
+
+
+
+
 
 def create_condition_txt(column):
     #TODO this can be implemented using map()
@@ -49,5 +104,5 @@ def create_text (df_transcription):
         text[column]=[create_condition_txt(df_transcription[column])]
 
     handle_pickle(args.output_dic,text)
-create_text(df)
+#create_text(df)
 

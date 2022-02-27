1af3834e5d7d3b122699e8f496b51ff37d2546d6
Yesid <yesidcanoc@gmail.com>
2022-02-27 16:44:49 -0500

    Deleted function create_text

--- NLP/transcription_preprocessing.py
+++ NLP/transcription_preprocessing.py
@@ -71,47 +71,6 @@ def process_rated_transcription (input_path, output_dic):
 
 
 
+process_rated_transcription(args.transcription_csv,args.output_dic)
 
-
-output_dic= '/home/yesid/Documents/Master_semester3/VR/text_analysis_task/clean_transcription/iteration_2'
-input_path = '/home/yesid/Documents/Master_semester3/VR/text_analysis_task/rated_transcription/iteration_2/df_8_long_files_processed_2.csv'
-
-process_rated_transcription(input_path,output_dic)
-
-
-
-
-
-def create_condition_txt(column):
-    #TODO this can be implemented using map()
-    text=''
-    for cell in column:
-        if type(cell) != float:
-            #TODO delete /n
-            text = f'{text}  \n {str(cell)}'
-            #text= text + '\n' + str(cell)
-    return text
-
-def create_text (df_transcription):
-    """
-    Creates a whole text from the transcriptions belonging to each condition.
-    Args:
-        df_transcription: Data frame containing the transcriptions
-
-    Returns:
-
-    """
-
-    #this regex returns the name of the column
-    column_regex = re.compile(r'([A-Za-z]*)\s?\_?([0-9]$)')
-    #list of the columns
-    columns = df_transcription.columns
-    columns=columns[1:-1]
-    #column_name= lambda count: column_regex.search(columns[count]).groups()[0]
-    text={}
-    for column in columns:
-        text[column]=[create_condition_txt(df_transcription[column])]
-
-    handle_pickle(args.output_dic,text)
-#create_text(df)
 
